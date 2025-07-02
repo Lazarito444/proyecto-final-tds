3531ff697db72bbbf683020d392e5f6591a72212
Ariel Lázaro <ariellazaro444@gmail.com>
2025-07-01 21:56:09 -0400

    Update ApplicationUser entity & table with more optional properties

---
+++ src/financia_backend/FinancIA.Infrastructure.Persistence/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using FinancIA.Core.Application.Identity;
+using FinancIA.Core.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinancIA.Infrastructure.Persistence.Configurations;
+public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(u => u.Email).IsRequired();
+
+        builder.Property(u => u.FullName).IsRequired()
+            .HasColumnType("VARCHAR(150)");
+
+        builder.Property(u => u.DateOfBirth)
+            .IsRequired(false)
+            .HasColumnType("DATE");
+
+        builder.Property(u => u.Gender)
+            .HasDefaultValue(Gender.Unspecified);
+
+        builder.Property(u => u.ImagePath)
+            .IsRequired(false)
+            .HasColumnType("VARCHAR(200)");
+    }
+}
---
+++ src/financia_backend/FinancIA.Infrastructure.Persistence/Migrations/20250702015441_UpdateUsersTable.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancIA.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "VARCHAR(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "DATE",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "AspNetUsers",
+                type: "VARCHAR(200)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_AspNetUsers_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_AspNetUsers_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+        }
+    }
+}
