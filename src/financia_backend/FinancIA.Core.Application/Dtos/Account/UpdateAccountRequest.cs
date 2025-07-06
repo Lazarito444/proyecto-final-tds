3aaf3fbbb4002025168af7620e1ab96698bc049b
Ariel Lázaro <ariellazaro444@gmail.com>
2025-07-06 14:09:18 -0400

    Add routes for public access to profile photos, implement update account endpoint

---
+++ src/financia_backend/FinancIA.Core.Application/Dtos/Account/UpdateAccountRequest.cs
@@ -0,0 +1,13 @@
+﻿using FinancIA.Core.Domain.Enums;
+using Microsoft.AspNetCore.Http;
+
+namespace FinancIA.Core.Application.Dtos.Account;
+public class UpdateAccountRequest
+{
+    public string? FullName { get; set; }
+    public DateOnly? DateOfBirth { get; set; }
+    public Gender? Gender { get; set; }
+    public string? Password { get; set; }
+    public string? CurrentPassword { get; set; }
+    public IFormFile? PhotoFile { get; set; }
+}
---
+++ src/financia_backend/FinancIA.Core.Application/Validators/Account/UpdateAccountRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FinancIA.Core.Application.Dtos.Account;
+using FluentValidation;
+
+namespace FinancIA.Core.Application.Validators.Account;
+public class UpdateAccountRequestValidator : AbstractValidator<UpdateAccountRequest>
+{
+    public UpdateAccountRequestValidator()
+    {
+        RuleFor(dto => dto.FullName).NotEmpty();
+    }
+}
