7fed8e921ceffd829a47b43bcd9204d058e58669
Ariel Lázaro <ariellazaro444@gmail.com>
2025-07-06 21:35:12 -0400

    Create endpoints for budget & saving entities

--- src/financia_backend/FinancIA.Core.Domain/Entities/Saving.cs
+++ src/financia_backend/FinancIA.Core.Domain/Entities/Saving.cs
@@ -1,11 +1,11 @@
 ﻿namespace FinancIA.Core.Domain.Entities;
-public class Savings
+public class Saving
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public Guid UserId { get; set; }
     public string Name { get; set; }
     public decimal GoalAmount { get; set; }
     public decimal CurrentAmount { get; set; }
-    public DateTime Deadline { get; set; }
+    public DateOnly? Deadline { get; set; }
     public bool Finished => CurrentAmount >= GoalAmount;
 }
