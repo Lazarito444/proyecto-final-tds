7d0302ca4f0cf79b10e2667581e1bcfc2483e9bd
Ariel Lázaro <ariellazaro444@gmail.com>
2025-08-12 23:09:42 -0400

    Ignore flutter suggestions

--- src/financia_mobile/lib/modules/categories/categories_screen.dart
+++ src/financia_mobile/lib/modules/categories/categories_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use, use_build_context_synchronously
+
 import 'package:financia_mobile/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:financia_mobile/extensions/theme_extensions.dart';
